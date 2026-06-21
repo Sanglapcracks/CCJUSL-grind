a02fdae6b7a15e366e8a8cf83cd5c2befd0925b7
aritra-mondal-it <maritra.it.ug@jadavpuruniversity.in>
2026-06-21 23:28:23 +0530

    Adding details of src

--- data/teams.ts
+++ data/teams.ts
@@ -44,6 +44,23 @@ export const teamMembers: TeamMember[] = [
     socials: { linkedin: "https://www.linkedin.com/in/sauhardya-hazra-b44110324", github: "https://github.com/Sauhardya007" },
   },
   {
+    id: "t5",
+    name: "Soumyadeep Roy Chowdhur",
+    position: "PR Coordinator",
+    photo: "/member_photos/src.jpeg", 
+    team: "pr",
+    graduationYear: 2029,
+    bio: "Tech enthusiast with hands-on experience architecting highly concurrent distributed systems, AI-integrated software. Passionate about tackling complex data structures, building scalable, high-performance web applications.",
+    achievements: [
+      "Ranked 46 & 146 in ICPC Asia West Chennai and Amritapuri Regionals 2025 respectively.",
+      "Reached Specialist in CODEFORCES with a Maximum Rating of 1448 and 3 Star on CODECHEF.",
+      "Solved over 500+ problems across all competitive programming platforms.",
+      "Finalist of Eastern India Blockchain Summit 2.0 at IIT Kharagpur.",
+      "Achieved 98%ile in JEE Mains 2024 a rank of 326 in WBJEE 2024."
+    ],
+    socials: { linkedin: "https://www.linkedin.com/in/soumyadeep-roy-chowdhury101", github: "https://github.com/soumyadeep-rc " },
+  },
+  {
     id: "t2",
     name: "Md Arsalan",
     position: "PR Coordinator",
